ec92d7c1c1dd49c6c55753bf0e26f77568b30ed0
Clint Nathan Calumpad <clintcalumpad@gmail.com>
2024-05-28 02:07:41 +0800

    feat: added scrolling to text display on the lcd when reading a card.

--- nfc-poll.py
+++ nfc-poll.py
@@ -2,8 +2,8 @@ from py532lib.i2c import *
 from py532lib.frame import *
 from py532lib.constants import *
 from rpi_lcd import LCD
-
 import logging
+from time import sleep
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
@@ -17,11 +17,24 @@ lcd.text("NFC Polling", 1)
 sleep(1)
 lcd.clear()
 lcd.text("Tap a card", 1)
-while True: 
-    
-    card_data = pn532.read_mifare().get_data()
-    card_data_formatted = ' '.join(format(x, '02X') for x in card_data)
-    nfc_uid = ' '.join(card_data_formatted.split()[7:])
-    logging.info(nfc_uid)
-    print(nfc_uid)
-    lcd.text(nfc_uid, 2)
+
+def scroll_text(text, row=2, delay=0.5):
+    # Scroll the text continuously
+    while True:
+        for i in range(len(text) - 13):
+            lcd.text(text[i:i+16], row)
+            sleep(delay)
+
+try:
+    while True:
+        card_data = pn532.read_mifare().get_data()
+        card_data_formatted = ' '.join(format(x, '02X') for x in card_data)
+        nfc_uid = ' '.join(card_data_formatted.split()[7:])
+        logging.info(nfc_uid)
+        print(nfc_uid)
+        scroll_text(nfc_uid, row=2, delay=0.7)
+
+except KeyboardInterrupt:
+    # Stop scrolling when Ctrl+C is pressed
+    lcd.clear()
+    lcd.text("Scrolling stopped", 2)
